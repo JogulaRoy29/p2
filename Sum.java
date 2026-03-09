03cdf2109067913fe2f0866688440ad14074f023
JogulaRoy29 <245123751304@mvsrec.edu.in>
2026-03-09 16:09:09 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,16 @@
+class Sum {
+    public static void main(String args[]) {
+        int num = 1234;
+        int sum = 0, rem;
+
+        System.out.println("Input: " + num);
+
+        while(num != 0) {
+            rem = num % 10;
+            sum = sum + rem;
+            num = num / 10;
+        }
+
+        System.out.println("Output: Sum of digits = " + sum);
+    }
+}
