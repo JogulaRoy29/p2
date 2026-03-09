26c99b285c7335fc606567bb846cf5df38621258
JogulaRoy29 <245123751304@mvsrec.edu.in>
2026-03-09 15:56:31 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,18 @@
+class Prime {
+    public static void main(String args[]) {
+        int num = 7;
+        int count = 0;
+
+        System.out.println("Input: " + num);
+
+        for(int i = 1; i <= num; i++) {
+            if(num % i == 0)
+                count++;
+        }
+
+        if(count == 2)
+            System.out.println("Output: Prime Number");
+        else
+            System.out.println("Output: Not Prime");
+    }
+}
