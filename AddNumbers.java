05d318215d7fe3cb622e9ffd34ace41b6d684fea
JogulaRoy29 <245123751304@mvsrec.edu.in>
2026-03-09 15:34:28 +0530

    Create AddNumbers.java

---
+++ AddNumbers.java
@@ -0,0 +1,15 @@
+class AddNumbers {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("Input:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        int sum = a + b;
+
+        System.out.println("Output:");
+        System.out.println("Sum = " + sum);
+    }
+}
