96a32ab42f9362cf35d60ed8e1e66dfa02e8b40d
JogulaRoy29 <245123751304@mvsrec.edu.in>
2026-03-09 15:38:47 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,12 @@
+class EvenOdd {
+    public static void main(String args[]) {
+        int num = 15;
+
+        System.out.println("Input: " + num);
+
+        if(num % 2 == 0)
+            System.out.println("Output: Even Number");
+        else
+            System.out.println("Output: Odd Number");
+    }
+}
