b02dfae5a1ab522eca06bb15d647bd34e2cb41cc
JogulaRoy29 <245123751304@mvsrec.edu.in>
2026-03-09 15:50:13 +0530

    Add multiplication table program in Table.java

---
+++ Table.java
@@ -0,0 +1,12 @@
+class Table {
+    public static void main(String args[]) {
+        int num = 5;
+
+        System.out.println("Input: " + num);
+        System.out.println("Output:");
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
